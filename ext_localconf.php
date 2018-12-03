bc955bbb9f79d6a598d0f57d1e5f3bb072d6ec57
Tizian Schmidlin <st@cabag.ch>
2018-12-03 13:24:56 +0100

    Fix namespace

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,6 +2,6 @@
 defined('TYPO3_MODE') or die ('Access denied.');
 
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][]
-    = 'Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderAll';
+    = \Ka\Http2Push\Hooks\ContentPostProcessor::class . '->renderAll';
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][]
-    = 'Ka\\Http2Push\\Hooks\\ContentPostProcessor->renderOutput';
+    = \Ka\Http2Push\Hooks\ContentPostProcessor::class . '->renderOutput';
